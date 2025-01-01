65bfd2cba9c11b1fd8b2375446249a3cddac80f6
OtwakO <tom28055101@gmail.com>
2025-01-02 06:35:49 +0800

    Update script

--- app/rename_series.py
+++ app/rename_series.py
@@ -11,20 +11,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 WATCH_DIRECTORY = [Path("watchlist")]
-WATCH_EXTENSION = (".mkv", ".mp4", ".nfo", ".avi")
-EXCLUDE_NAME = ("tvshow.nfo", "season.nfo")
+WATCH_EXTENSION = os.getenv("WATCH_EXTENSION", ".mp4;.mkv;.avi;.nfo;.srt").split(";")
+EXCLUDE_FILE = os.getenv("EXCLUDE_FILE").split(";")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash-8b")
 
-SYSTEM_PROMPT = """
-You will be assessing TV shows or animes' season number and episodes based on filepaths provided to you.
-Response as JSON format with the following JSON schema without markdowns or markups:
+SYSTEM_PROMPT = f"""
+You will be assessing TV shows or animes' with file extensions in {WATCH_EXTENSION} for it's episode information based on the filepaths provided to you.
+Show name should not include season number or episode number.
+Respond in JSON format with the following JSON schema:
 
-file_to_rename = {"file_path": str, "show_name": str, "season_number": int, "episode_number": int,  "is_special_season": bool}
-Return: {"files": list[file_to_rename]}
+episode_info = {{"file_path": str, "show_name": str, "season_number": int, "episode_number": int,  "is_special_season": bool}}
+Return: list[episode_info]
 """
 
 def rename_series(filenames: list[str]):
@@ -77,12 +78,14 @@ def start_rename(watch_directory: list[Path]):
             for episode in series.rglob("*")
             if episode.suffix in WATCH_EXTENSION
             and not is_renamed(episode.name)
-            and episode.name not in EXCLUDE_NAME
+            and episode.name not in EXCLUDE_FILE
         ]
         if episodes:
             ai_results = rename_series(episodes)
-            for episode in ai_results["files"]:
-                rename_episode(episode["file_path"], episode["show_name"], episode["season_number"], episode["episode_number"], episode["is_special_season"])
+            for episode_info in ai_results:
+                relative_path = Path(episode_info["file_path"]).relative_to(Path("watchlist"))
+                if match := next((real_path for real_path in episodes if str(relative_path) in str(real_path)), None):
+                    rename_episode(match, episode_info["show_name"], episode_info["season_number"], episode_info["episode_number"], episode_info["is_special_season"])
             if index == len(all_folders) - 1:  # Check if last iteration
                 break
             else:
@@ -91,5 +94,5 @@ def start_rename(watch_directory: list[Path]):
 
 if __name__ == "__main__":
     start_rename(WATCH_DIRECTORY)
-    # test = ["[Sakurato] Sousou no Frieren Season 2 [22][AVC-8bit 1080p AAC][CHT]", "/Season 3/[Nekomoe kissaten&LoliHouse] Kono Sekai wa Fukanzen Sugiru - 04 [WebRip 1080p HEVC-10bit AAC ASSx2].mkv"]
-    # print(type(rename_series(test)))
+    # test = ["/bangumi/[Sakurato] Sousou no Frieren Season 2 [22][AVC-8bit 1080p AAC][CHT].mp4", "/Season 3/[Nekomoe kissaten&LoliHouse] Kono Sekai wa Fukanzen Sugiru - 04 [WebRip 1080p HEVC-10bit AAC ASSx2].mkv"]
+    # print(rename_series(test))
