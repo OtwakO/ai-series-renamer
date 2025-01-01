ba685a2f552f42099e962dc5e0fb1492cdc5b509
OtwakO <tom28055101@gmail.com>
2025-01-02 05:15:30 +0800

    Update prompt

--- app/rename_series.py
+++ app/rename_series.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import google.generativeai as genai
 import msgspec
+import typing_extensions as typing
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -19,19 +20,12 @@ genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash-8b")
 
 SYSTEM_PROMPT = """
-You will be renaming TV shows, animes from it's filename(s) for me.
+You will be assessing TV shows or animes' season number and episodes based on filepaths provided to you.
+Response as JSON format with the following JSON schema without markdowns or markups:
 
-Provide a new filename for each filename I provide to you, filenames will be provided to you in python list format, following the format of {{Show name}} S{{Season number}}E{{Episode number}}. Season number and episode number should be padded to two digits if only single digit.
-Your response should be in JSON format with no markups like the following and nothing else should be included:
-
-
-Use this JSON schema: {"{{Original filename}}": "{{New filename}}"}
-Input: ["[Sakurato] Yuru Camp Season 2 [13][AVC-8bit 1080p AAC][CHT]"]
-Output: {"[Sakurato] Yuru Camp Season 2 [13][AVC-8bit 1080p AAC][CHT]": "Yuru Camp S02E13"}
-
-DO NOT INCLUDE ANY FORM OF MARKUP IN YOUR RESPONSE, SUCH AS ```json```, THIS IS IMPORTANT.
+file_to_rename = {"file_path": str, "show_name": str, "season_number": int, "episode_number": int,  "is_special_season": bool}
+Return: {"files": list[file_to_rename]}
 """
-
 
 def rename_series(filenames: list[str]):
     response = model.generate_content([SYSTEM_PROMPT, str(filenames)])
@@ -40,7 +34,7 @@ def rename_series(filenames: list[str]):
     try:
         response_dict = msgspec.json.decode(response_data)
     except ValueError as e:
-        print(f"Invalid JSON format, please provide a valid JSON response. {e}")
+        print(f"Invalid JSON format from response: {response.text}")
     return response_dict
 
 
@@ -50,6 +44,17 @@ def is_renamed(episode_name: str):
         return True
     return False
 
+def rename_episode(episode_path: Path, show_name: str, season_number: int, episode_number: int, is_special_season: bool):
+    file = Path(episode_path)
+    if file.is_file():
+        if is_special_season:
+            new_filename = f"{show_name} S00E{episode_number:02d}{file.suffix}"
+        else:
+            new_filename = f"{show_name} S{season_number:02d}E{episode_number:02d}{file.suffix}"
+        file.rename(Path(file.parent, new_filename))
+        print(f"Renaming: {str(file)} -> {str(Path(file.parent, new_filename))}\n")
+    else:
+        print(f"Unable to rename {str(file)}, file doesn't exist?\n")
 
 def start_rename(watch_directory: list[Path]):
     all_folders = []
@@ -75,23 +80,9 @@ def start_rename(watch_directory: list[Path]):
             and episode.name not in EXCLUDE_NAME
         ]
         if episodes:
-            episode_names = [episode.stem for episode in episodes]
-            ai_results = rename_series(episode_names)
-            for episode in episodes:
-                parent_directory = episode.parent
-                if ai_results.get(episode.stem):
-                    new_filename = f"{ai_results[episode.stem]}{episode.suffix}"
-                    episode.rename(
-                        Path(
-                            parent_directory,
-                            new_filename,
-                        )
-                    )
-                    print(
-                        f"Renaming: {str(episode)} -> {str(Path(parent_directory, new_filename))}\n"
-                    )
-                else:
-                    print(f"Unable to rename {str(episode)}\n")
+            ai_results = rename_series(episodes)
+            for episode in ai_results["files"]:
+                rename_episode(episode["file_path"], episode["show_name"], episode["season_number"], episode["episode_number"], episode["is_special_season"])
             if index == len(all_folders) - 1:  # Check if last iteration
                 break
             else:
@@ -99,6 +90,6 @@ def start_rename(watch_directory: list[Path]):
 
 
 if __name__ == "__main__":
-    # start_rename(WATCH_DIRECTORY)
-    test = ["[Sakurato] Sousou no Frieren [26][AVC-8bit 1080p AAC][CHT]"]
-    print(rename_series(test))
+    start_rename(WATCH_DIRECTORY)
+    # test = ["[Sakurato] Sousou no Frieren Season 2 [22][AVC-8bit 1080p AAC][CHT]", "/Season 3/[Nekomoe kissaten&LoliHouse] Kono Sekai wa Fukanzen Sugiru - 04 [WebRip 1080p HEVC-10bit AAC ASSx2].mkv"]
+    # print(type(rename_series(test)))
