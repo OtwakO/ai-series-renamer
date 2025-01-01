45dab8cc9c0f35285a8c7fd52ce72ca715dbdb59
OtwakO <tom28055101@gmail.com>
2025-01-02 07:06:22 +0800

    Fix bug

--- app/rename_series.py
+++ app/rename_series.py
@@ -10,7 +10,7 @@ import typing_extensions as typing
 from dotenv import load_dotenv
 
 load_dotenv()
-WATCH_DIRECTORY = [Path("watchlist")]
+WATCH_DIRECTORY = Path("watchlist")
 WATCH_EXTENSION = os.getenv("WATCH_EXTENSION", ".mp4;.mkv;.avi;.nfo;.srt").split(";")
 EXCLUDE_FILE = os.getenv("EXCLUDE_FILE", "tvshow.nfo;season.nfo").split(";")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
@@ -53,21 +53,20 @@ def rename_episode(episode_path: Path, show_name: str, season_number: int, episo
         else:
             new_filename = f"{show_name} S{season_number:02d}E{episode_number:02d}{file.suffix}"
         file.rename(Path(file.parent, new_filename))
-        print(f"Renaming: {str(file)} -> {str(Path(file.parent, new_filename))}\n")
+        print(f"Renaming: {str(file.relative_to(WATCH_DIRECTORY))} -> {str(Path(file.parent.relative_to(WATCH_DIRECTORY), new_filename))}\n")
     else:
         print(f"Unable to rename {str(file)}, file doesn't exist?\n")
 
-def start_rename(watch_directory: list[Path]):
+def start_rename():
     all_folders = []
     print("-" * 50)
     print(f"Run started at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
     print("Watching directory: ")
-    for path in watch_directory:
-        if path.is_dir():
-            detect_folders = [
-                Path(folder) for folder in path.iterdir() if folder.is_dir()
-            ]
-            all_folders.extend(detect_folders)
+    if WATCH_DIRECTORY.is_dir():
+        detect_folders = [
+            Path(folder) for folder in WATCH_DIRECTORY.iterdir() if folder.is_dir()
+        ]
+        all_folders.extend(detect_folders)
 
     for folder in all_folders:
         print(f"- {folder.name}")
@@ -83,11 +82,8 @@ def start_rename(watch_directory: list[Path]):
         if episodes:
             ai_results = rename_series(episodes)
             for episode_info in ai_results:
-                print(episodes)
                 relative_path = Path(episode_info["file_path"]).relative_to(Path("/watchlist"))
                 if match := next((real_path for real_path in episodes if str(relative_path) in str(real_path)), None):
-                    print(episode_info["file_path"])
-                    print(match)
                     rename_episode(match, episode_info["show_name"], episode_info["season_number"], episode_info["episode_number"], episode_info["is_special_season"])
             if index == len(all_folders) - 1:  # Check if last iteration
                 break
@@ -96,7 +92,7 @@ def start_rename(watch_directory: list[Path]):
 
 
 if __name__ == "__main__":
-    start_rename(WATCH_DIRECTORY)
+    start_rename()
     # print(Path("/watchlist/bangumi/test/Season 4/[Sakurato] Sousou no Frieren [21][AVC-8bit 1080p AAC][CHT].nfo").relative_to(Path("/watchlist")))
     # test = ["/bangumi/[Sakurato] Sousou no Frieren Season 2 [22][AVC-8bit 1080p AAC][CHT].mp4", "/Season 3/[Nekomoe kissaten&LoliHouse] Kono Sekai wa Fukanzen Sugiru - 04 [WebRip 1080p HEVC-10bit AAC ASSx2].mkv"]
     # print(rename_series(test))
